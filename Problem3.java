7e4c91f55bd3bb0b1361582ee0cdb5afb9be8f9c
TeaFred <fhtruong100@student.hbuhsd.edu>
2024-10-13 05:20:28 +0000

    completed?

--- Problem3.java
+++ Problem3.java
@@ -5,18 +5,20 @@ public class Problem3
   public static void main(String[] args)
   {
     Scanner scan = new Scanner(System.in);
-    integer sides;
+    Integer sides;
     Double length;
     
-    System.out.println("Enter number of sides:");
-    sides = scan.nextInteger();
-    System.out.println("Enter side length:");
+    System.out.println("Enter number of sides: ");
+    sides = scan.nextInt();
+    System.out.println("Enter side length: ");
     length = scan.nextDouble();
     
-    RegularPolygon p1 = RegularPolygon(length, sides);
-    RegularPolygon p2 = RegularPolygon(length + 1, sides * 2);
+    RegularPolygon p1 = new RegularPolygon(length, sides);
+    RegularPolygon p2 = new RegularPolygon();
+    p2.setSideLength(length*2);
+    p2.setNumSides(sides+2);
     
-    System.out.println("The area of a " + p1 + " is: " + p1.getArea());
-    System.out.println("The area of a " + p2 + " is: " + p2.getArea());
+    System.out.println("The area of a " + p1.toString() + " with side length " + p1.getSideLength() + " is: " + p1.getArea());
+    System.out.println("The area of a " + p2.toString() + " with side length " + p2.getSideLength() + " is: " + p2.getArea());
   }
 }
